1bed5254267ee5bd78cdb892c53627dc2b969833
Azat Yarullin <azat.yarullin@gmail.com>
2024-05-02 03:25:32 +0300

    moisture

--- get_data.py
+++ get_data.py
@@ -1,14 +1,17 @@
 import time
 from flask import Flask, Response
-from prometheus_client import Counter, Gauge, start_http_server, generate_latest
+from prometheus_client import Gauge, generate_latest
 
 import busio
 import board
 
 from adafruit_htu21d import HTU21D
+import adafruit_ads1x15.ads1115 as ads1115
+from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = HTU21D(i2c)
+ads = ads1115.ADS1115(i2c)
 
 content_type = str('text/plain; version=0.0.4; charset=utf-8')
 
@@ -26,11 +29,30 @@ current_temperature = Gauge(
     ['room']
 )
 
+current_moisture_pot1 = Gauge(
+    'current_moisture_pot1',
+    'plant box one',
+    ['room']
+)
+
+current_moisture_pot2 = Gauge(
+    'current_moisture_pot2',
+    'plant box two',
+    ['room']
+)
+
 
 def get_sensors_readings():
     humidity = format(sensor.relative_humidity, ".2f")
     temperature = format(sensor.temperature, ".2f")
-    response = {"temperature": temperature, "humidity": humidity}
+    moisture_pot1 = format(AnalogIn(ads, ads1115.P0).value, ".2f")
+    moisture_pot2 = format(AnalogIn(ads, ads1115.P1).value, ".2f")
+    response = {
+        "temperature": temperature,
+        "humidity": humidity,
+        "moisture_pot1": moisture_pot1,
+        "moisture_pot2": moisture_pot2
+    }
     return response
 
 
@@ -39,6 +61,8 @@ def metrics():
     metrics = get_sensors_readings()
     current_humidity.labels('balcony').set(metrics['humidity'])
     current_temperature.labels('balcony').set(metrics['temperature'])
+    current_moisture_pot1.labels('balcony').set(metrics['moisture_pot1'])
+    current_moisture_pot2.labels('balcony').set(metrics['moisture_pot2'])
     return Response(generate_latest(), mimetype=content_type)
 
 
