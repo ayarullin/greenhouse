e4c082f55c03f12bdf3ee7b01522c5b70d6d8d29
Azat Yarullin <azat.yarullin@gmail.com>
2024-05-06 06:46:42 +0300

    solar data

--- get_data.py
+++ get_data.py
@@ -31,7 +31,7 @@ current_temperature = Gauge(
 
 current_moisture_pot1 = Gauge(
     'current_moisture_pot1',
-    'plant box one',
+    'plnt box one',
     ['room']
 )
 
@@ -41,17 +41,25 @@ current_moisture_pot2 = Gauge(
     ['room']
 )
 
+current_solar_state = Gauge(
+    'current_solar_state',
+    'SOLAR',
+    ['room']
+)
+
 
 def get_sensors_readings():
     humidity = format(sensor.relative_humidity, ".2f")
     temperature = format(sensor.temperature, ".2f")
     moisture_pot1 = format(AnalogIn(ads, ads1115.P0).value, ".2f")
     moisture_pot2 = format(AnalogIn(ads, ads1115.P1).value, ".2f")
+    solar_state = format(AnalogIn(ads, ads1115.P2).value, ".2f")
     response = {
         "temperature": temperature,
         "humidity": humidity,
         "moisture_pot1": moisture_pot1,
-        "moisture_pot2": moisture_pot2
+        "moisture_pot2": moisture_pot2,
+        "solar_state": solar_state
     }
     return response
 
@@ -63,6 +71,7 @@ def metrics():
     current_temperature.labels('balcony').set(metrics['temperature'])
     current_moisture_pot1.labels('balcony').set(metrics['moisture_pot1'])
     current_moisture_pot2.labels('balcony').set(metrics['moisture_pot2'])
+    current_solar_state.labels('balcony').set(metrics['solar_state'])
     return Response(generate_latest(), mimetype=content_type)
 
 
